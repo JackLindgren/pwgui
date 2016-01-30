b6e5d7dc8b34c5228c86a6e392dfca596dcdef13
allabouttoledo <jtlindgren88@gmail.com>
2016-01-30 12:59:31 -0800

    created frames

--- myFields.py
+++ myFields.py
@@ -1,24 +1,10 @@
 from Tkinter import *
 
-# this will explain roughly how to do pages
-# http://stackoverflow.com/questions/14817210
-master = Tk()
-#asdf
-entries = []
+def raise_frame(frame):
+	frame.tkraise()
 
-nLabel = Label(master, text="Name")
-eLabel = Label(master, text="Email")
-pLabel = Label(master, text="Password")
-nLabel.grid(row=0)
-eLabel.grid(row=1)
-pLabel.grid(row=2)
-
-name = Entry(master)
-email = Entry(master)
-password = Entry(master, show="*")
-name.grid(row=0, column=1)
-email.grid(row=1,column=1)
-password.grid(row=2, column=1)
+def goBack():
+	raise_frame(loginPage)
 
 def callback():
 	if email.get() and name.get() and password.get():
@@ -27,16 +13,49 @@ def callback():
 		email.delete(0,1000)
 		password.delete(0,1000)
 	print entries
+	raise_frame(sucesPage)
 
 def clearText():
 	name.delete(0,1000)
 	email.delete(0, 1000)
 	password.delete(0, 1000)
 
-b = Button(master, text="ENTER", width=10, command=callback)
+master = Tk()
+
+loginPage = Frame(master)
+sucesPage = Frame(master)
+
+for frame in (loginPage, sucesPage):
+	frame.grid(row=0, column=0, sticky='news')
+
+entries = []
+
+# create the labels
+nLabel = Label(loginPage, text="Name")
+eLabel = Label(loginPage, text="Email")
+pLabel = Label(loginPage, text="Password")
+nLabel.grid(row=0)
+eLabel.grid(row=1)
+pLabel.grid(row=2)
+
+sLabel = Label(sucesPage, text="Success!")
+sLabel.grid(row=0)
+rButton = Button(sucesPage, text="RETURN", width=10, command=goBack)
+rButton.grid(row=1)
+
+# create the entries:
+name = Entry(loginPage)
+email = Entry(loginPage)
+password = Entry(loginPage, show="*")
+name.grid(row=0, column=1)
+email.grid(row=1,column=1)
+password.grid(row=2, column=1)
+
+b = Button(loginPage, text="ENTER", width=10, command=callback)
 b.grid(row=3, column=0)
 
-c = Button(master, text="CLEAR", width=10, command=clearText)
+c = Button(loginPage, text="CLEAR", width=10, command=clearText)
 c.grid(row=3, column=1)
 
+raise_frame(loginPage)
 mainloop()
