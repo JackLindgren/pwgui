5bfdd9288741dae9e546b4b986cab38ca082c867
allabouttoledo <jtlindgren88@gmail.com>
2016-01-30 17:31:29 -0800

    added trace of page status

--- ooPages.py
+++ ooPages.py
@@ -1,9 +1,8 @@
 import Tkinter as tk
+from threading import Thread
+import time
 
 entries = []
-
-def show_page(page):
-	page.tkraise()
 
 class Page(tk.Frame):
 	def __init__(self, *args, **kwargs):
@@ -15,19 +14,31 @@ class LoginScreen(Page):
 	def __init__(self, *args, **kwargs):
 		Page.__init__(self, *args, **kwargs)
 
+		# using trace on the status as a wonky way to get changes when a form is submitted
+		# self.status = tk.IntVar()
+		# self.status.set(1)
+		# print self.status.get()
+
+		self.status = tk.StringVar()
+		self.status.set("working")
+		print self.status.get()
+
 		def clearText():
 			name.delete(0,1000)
 			email.delete(0,1000)
 			password.delete(0,1000)
 		
 		def submitEntry():
+			print "login status before submission", self.status.get()
 			if email.get() and name.get() and password.get():
 				entries.append([email.get(), name.get(), password.get()])
 				clearText()
 				print entries
-				# success = SuccessScreen(self)
-				# success.show()
-
+				self.status.set("done")
+				print self.status.get()
+				# self.status.set(0)	# change the status - this will prompt trace, which will call to go to the success screen
+									# we only want to change the status if the form was submitted
+									# otherwise, let the status stay the same	
 		# labels
 		nLabel = tk.Label(self, text="Name")
 		eLabel = tk.Label(self, text="Email")
@@ -53,32 +64,61 @@ class LoginScreen(Page):
 class SuccessScreen(Page):
 	def __init__(self, *args, **kwargs):
 		Page.__init__(self, *args, **kwargs)
+		self.status = tk.IntVar()
+		self.status.set(1)
+
+		def goBackLogin():
+			self.status.set(0)
+			self.status.set(1)
+
+		def continueGoing():
+			self.status.set(2)
+
 		message = tk.Label(self, text="Success!")
-		retButton = tk.Button(self, text="Go Back", width=10)
+		retButton = tk.Button(self, text="Go Back", width=10, command=goBackLogin)
 		retButton.pack()
+		continueButton = tk.Button(self, text="continue", width=10, command=continueGoing)
+		continueButton.pack()
+
+
+class NextScreen(Page):
+	def __init__(self, *args, **kwargs):
+		Page.__init__(self, *args, **kwargs)
+		message = tk.Label(self, text="Third page")
+		message.pack()
 
 class MainView(tk.Frame):
 	def __init__(self, *args, **kwargs):
 		tk.Frame.__init__(self, *args, **kwargs)
+
+		def goToSuccess(arg1, arg2, arg3):
+			if login.status.get() == "done":		# if the login page's status was changed (if the form was submitted)
+				success.show()				# show the success page
+
+		def afterLogin(arg1,arg2,arg3):
+			if success.status.get() == 1:
+				login.show()
+				login.status.set("working")
+				print "login status", login.status.get()
+			elif success.status.get() == 2:
+				third.show()
+				print "login status", login.status.get()
+
 		login = LoginScreen(self)
 		success = SuccessScreen(self)
+		third = NextScreen(self)
 
 		container = tk.Frame(self)
 		container.pack(side="top", fill="both", expand=True)
-		
-		login.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+		login.place(in_=container,   x=0, y=0, relwidth=1, relheight=1)
 		success.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+		third.place(in_=container, 	 x=0, y=0, relwidth=1, relheight=1)
+
+		login.status.trace('w', goToSuccess)		# if login status is changed, go to success
+		success.status.trace('w', afterLogin)	# if succes's status is changed, return to login
 
 		login.show()
-		success.show()
-
-		# login.show()
-		# show(login)
-
-# show_page(LoginScreen)
-# show(LoginScreen)
-
-# mainloop()
 
 if __name__ == "__main__":
 	root = tk.Tk()
